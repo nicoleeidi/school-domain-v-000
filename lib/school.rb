49330b823ac432d9d668c46d47c19f0efc487ffe
Nicole Eidi <nicoleeidi1@gmail.com>
2018-01-28 18:36:51 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+
+  def initialize(name)
+      @name=name
+      @roster= {}
+  end
+
+  def add_student(name,grade)
+    if @roster.has_key?(grade) == false
+      @roster[grade]=[]
+      @roster[grade]<<name
+    else
+      @roster[grade]<< name
+    end
+  end
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    return @roster.keys.each do |hi|
+      @roster.hi.sort
+    end 
+  end
+end
